478bbea54f832f55b9c029aea9d97f9a63e96ca6
Jigang Kim <jgkim2020@snu.ac.kr>
2020-10-29 11:12:18 +0900

    Code cleanup

--- multiprocessing_example.py
+++ multiprocessing_example.py
@@ -12,98 +12,11 @@ from types import SimpleNamespace
 import urllib.request
 
 
-# def make_server_manager(port, authkey):
-#     """ Create a manager for the server, listening on the given port.
-#         Return a manager object with get_job_q and get_result_q methods.
-#     """
-#     job_q = Queue.Queue()
-#     result_q = Queue.Queue()
-
-#     # This is based on the examples in the official docs of multiprocessing.
-#     # get_{job|result}_q return synchronized proxies for the actual Queue
-#     # objects.
-#     class JobQueueManager(SyncManager):
-#         pass
-
-#     JobQueueManager.register('get_job_q', callable=lambda: job_q)
-#     JobQueueManager.register('get_result_q', callable=lambda: result_q)
-
-#     manager = JobQueueManager(address=('', port), authkey=authkey)
-#     manager.start()
-#     print 'Server started at port %s' % port
-#     return manager
-
-
-# def make_nums(N):
-#     """ Create N large numbers to factorize.
-#     """
-#     nums = [999999999999]
-#     for i in xrange(N):
-#         nums.append(nums[-1] + 2)
-#     return nums
-
-
-# def runserver():
-#     # Start a shared manager server and access its queues
-#     manager = make_server_manager(PORTNUM, AUTHKEY)
-#     shared_job_q = manager.get_job_q()
-#     shared_result_q = manager.get_result_q()
-
-#     N = 999
-#     nums = make_nums(N)
-
-#     # The numbers are split into chunks. Each chunk is pushed into the job
-#     # queue.
-#     chunksize = 43
-#     for i in range(0, len(nums), chunksize):
-#         shared_job_q.put(nums[i:i + chunksize])
-
-#     # Wait until all results are ready in shared_result_q
-#     numresults = 0
-#     resultdict = {}
-#     while numresults < N:
-#         outdict = shared_result_q.get()
-#         resultdict.update(outdict)
-#         numresults += len(outdict)
-
-#     # Sleep a bit before shutting down the server - to give clients time to
-#     # realize the job queue is empty and exit in an orderly way.
-#     time.sleep(2)
-#     manager.shutdown()
-
-
-# def runclient():
-#     manager = make_client_manager(IP, PORTNUM, AUTHKEY)
-#     job_q = manager.get_job_q()
-#     result_q = manager.get_result_q()
-#     mp_factorizer(job_q, result_q, 4)
-
-
-# def make_client_manager(ip, port, authkey):
-#     """ Create a manager for a client. This manager connects to a server on the
-#         given address and exposes the get_job_q and get_result_q methods for
-#         accessing the shared queues from the server.
-#         Return a manager object.
-#     """
-#     class ServerQueueManager(SyncManager):
-#         pass
-
-#     ServerQueueManager.register('get_job_q')
-#     ServerQueueManager.register('get_result_q')
-
-#     manager = ServerQueueManager(address=(ip, port), authkey=authkey)
-#     manager.connect()
-
-#     print 'Client connected to %s:%s' % (ip, port)
-#     return manager
-
-
-job_q = queue.Queue()
-result_q = queue.Queue()
-
 class Server(SyncManager):
     
     counter = 0
+    job_q = queue.Queue()
+    result_q = queue.Queue()
 
     def __init__(self, port, authkey, ip_type='local'):
         if ip_type == 'public':
@@ -153,7 +66,7 @@ class Server(SyncManager):
                 print('%d out of %d jobs done'%(numresults,N))
             except queue.Empty:
                 pass
-            time.sleep(1)
+            time.sleep(0.01)
 
         # Sleep a bit before shutting down the server - to give clients time to
         # realize the job queue is empty and exit in an orderly way.
@@ -173,8 +86,8 @@ class Server(SyncManager):
     def __del__(self):
         Server.counter -= 1
 
-Server.register('get_job_q', callable=lambda: job_q)
-Server.register('get_result_q', callable=lambda: result_q)
+Server.register('get_job_q', callable=lambda: Server.job_q)
+Server.register('get_result_q', callable=lambda: Server.result_q)
 
 
 class Client(SyncManager):
@@ -235,6 +148,13 @@ Client.register('get_job_q')
 Client.register('get_result_q')
 
 
+class Manager(SyncManager):
+    pass
+
+Manager.register('get_job_q')
+Manager.register('get_result_q')
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     list_of_identities = ['server', 'client']
@@ -254,8 +174,11 @@ if __name__ == "__main__":
     if args.identity == 'server':
         server = Server(params.port, str.encode(params.authkey), ip_type=getattr(params,'ip_type','local'))
         server.start()
+        stime = time.time()
         results = server.run()
+        ftime = time.time()
         print(results)
+        print(ftime - stime, 'seconds')
     elif args.identity == 'client':
         client = Client(params.ip, params.port, str.encode(params.authkey))
         client.connect()
